6d0f6ba0379ca9bcec5d526b129c78e3b3ae1c5c
lamtran03072000 <tranhoanglam03072000@gmail.com>
2024-06-17 18:49:49 +0700

    .

--- src/pages/ExRedux/PhoneShop/ListPhone.jsx
+++ src/pages/ExRedux/PhoneShop/ListPhone.jsx
@@ -1,102 +1,25 @@
 import React from 'react';
-
-import { Card, Col, Row } from 'antd';
+import { Card } from 'antd';
 const { Meta } = Card;
-
 const ListPhone = () => {
   return (
     <div>
-      <Row gutter={[20, 20]}>
-        <Col span={6}>
-          <Card
-            hoverable
-            style={{
-              width: '100%',
-            }}
-            cover={
-              <img
-                alt="example"
-                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-              />
-            }
-          >
-            <p>Tên sản phẩm</p>
-            <button className="btn btn-success">Mua Hàng</button>
-          </Card>
-        </Col>
-
-        <Col span={6}>
-          <Card
-            hoverable
-            style={{
-              width: '100%',
-            }}
-            cover={
-              <img
-                alt="example"
-                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-              />
-            }
-          >
-            <p>Tên sản phẩm</p>
-            <button className="btn btn-success">Mua Hàng</button>
-          </Card>
-        </Col>
-
-        <Col span={6}>
-          <Card
-            hoverable
-            style={{
-              width: '100%',
-            }}
-            cover={
-              <img
-                alt="example"
-                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-              />
-            }
-          >
-            <p>Tên sản phẩm</p>
-            <button className="btn btn-success">Mua Hàng</button>
-          </Card>
-        </Col>
-
-        <Col span={6}>
-          <Card
-            hoverable
-            style={{
-              width: '100%',
-            }}
-            cover={
-              <img
-                alt="example"
-                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-              />
-            }
-          >
-            <p>Tên sản phẩm</p>
-            <button className="btn btn-success">Mua Hàng</button>
-          </Card>
-        </Col>
-
-        <Col span={6}>
-          <Card
-            hoverable
-            style={{
-              width: '100%',
-            }}
-            cover={
-              <img
-                alt="example"
-                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-              />
-            }
-          >
-            <p>Tên sản phẩm</p>
-            <button className="btn btn-success">Mua Hàng</button>
-          </Card>
-        </Col>
-      </Row>
+      <Card
+        hoverable
+        style={{
+          width: 240,
+        }}
+        cover={
+          <img
+            alt="example"
+            height={200}
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+        }
+      >
+        <p>Tên sản phẩm</p>
+        <button className="btn btn-success">Mua ngay</button>
+      </Card>
     </div>
   );
 };
